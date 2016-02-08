a3f18b80aa9e7bccb9acb6df4bc814ee080755c8
Jon Mason <jon.mason@medsci.ox.ac.uk>
2016-02-08 15:57:46 +0000

    Added email to marker if report they failed is resubmitted

--- src/Controller/ReportsController.php
+++ src/Controller/ReportsController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Datasource\ConnectionManager;
+use Cake\Mailer\Email;
 
 /**
  * Reports Controller
@@ -140,6 +141,36 @@ class ReportsController extends AppController
 							$this->infolog("Report Save failed (old report). Attempt: " . $attemptId . "; Type: " . $type);
 							return false;
 						}
+						
+						//If this is a submission, and the user has already been marked, then this is a resubmission, so notify the marker
+						if($type === 'submit') {
+							$userId = $this->Auth->user('id');
+							$session = $this->request->session();	//Set Session to variable
+							$ltiResourceId = $session->read('LtiResource.id');
+							
+							$markQuery = $this->Reports->Attempts->LtiResources->Marks->find('all', [
+								'conditions' => ['Marks.lti_resource_id' => $ltiResourceId, 'Marks.lti_user_id' => $userId, 'Marks.mark' === 'Fail', 'Marks.revision' => 0],
+								'order' => ['Marks.modified' => 'DESC'],
+								'contain' => ['Marker', 'LtiUsers'],
+							]);
+							
+							if(!$markQuery->isEmpty()) {
+								$mark = $markQuery->first();
+								//pr($mark);
+								$message = '<div style="font-family: Verdana, Tahoma, sans-serif; font-size: 12px;"><p>Dear ' . $mark->marker->lti_lis_person_name_given . ',</p><p>' . $mark->marker->lti_lis_person_name_full . ', whose Viral Outbreak iCase report you marked as a fail, has resubmitted their report for remarking.</p><p>Please <a href="https://weblearn.ox.ac.uk/access/basiclti/site/8dd25ab4-a0ca-4e16-0073-d2a9667b58ce/content:122">go to the iCase</a> (<a href="https://weblearn.ox.ac.uk/access/basiclti/site/8dd25ab4-a0ca-4e16-0073-d2a9667b58ce/content:122">https://weblearn.ox.ac.uk/access/basiclti/site/8dd25ab4-a0ca-4e16-0073-d2a9667b58ce/content:122</a>) and remark this student. You can filter the marks to show only those that failed to make it easier to find this student.</p><p>Thank you!</p></div>';
+								//pr($message);
+								//exit;
+								
+								$email = new Email('smtp');
+								$email->from(['msdlt@medsci.ox.ac.uk' => 'Viral Outbreak iCase Admin'])
+									//->to('jon.mason@medsci.ox.ac.uk')	
+									->to($mark->marker->lti_lis_person_contact_email_primary)
+									->subject('Report Resubmitted for Remarking')
+									->emailFormat('html')
+									->send($message);
+							}
+						}
+
 						$this->set('status', 'success');
 						$this->infolog("Report Save succeeded. Attempt: " . $attemptId . "; Type: " . $type);
 					});
